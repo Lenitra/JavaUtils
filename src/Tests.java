e7046a3d1a4606406b6cd6de705b181aa4d4f4aa
Thomas L <lenitramc@gmail.com>
2025-02-26 09:25:53 +0100

    Update Tests.java

--- src/Tests.java
+++ src/Tests.java
@@ -2,25 +2,29 @@ import utils.*;
 
 public class Tests {
     public static void main(String[] args) {
-        // System.out.println(ReadVar.readInt("Saisir un entier : "));
-        // System.out.println(ReadVar.readBoolean("Saisir un boolean : "));
-        // System.out.println(ReadVar.readDouble("Saisir un double : "));
-        // System.out.println(ReadVar.readString("Saisir une chaine de caractères : "));
-        // System.out.println(ReadVar.readChar("Saisir un caractère : "));
+        // int entier = ReadVar.readInt("Veuillez saisir un entier : ");
+        // double decimal = ReadVar.readDouble("Veuillez saisir un double : ");
+        // String texte = ReadVar.readString("Veuillez saisir une chaîne de caractères : ");
+        // char caractere = ReadVar.readChar("Veuillez saisir un caractère : ");
+        // boolean booleen = ReadVar.readBoolean("Veuillez saisir un booléen (y/n) : ");
+
+        // System.out.println("Entier saisi : " + entier);
+        // System.out.println("Double saisi : " + decimal);
+        // System.out.println("Texte saisi : " + texte);
+        // System.out.println("Caractère saisi : " + caractere);
+        // System.out.println("Booléen saisi : " + booleen);
 
 
-        String[] menu = {"Banane", "Cerises", "Kiwi", "Pomme", "Raisin"};
-        MenuCreator.displayMenu(menu, "---- MENU 1 ----");
-        int choix = MenuCreator.readChoice(menu);
-        System.out.println("Vous avez choisi : " + menu[choix - 1]);
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
 
-        choix = MenuCreator.displayMenuAndReadChoice(menu, "---- MENU 2 ----");
-        System.out.println("Vous avez choisi : " + menu[choix - 1]);
+        // String[] menu = {"Banane", "Cerises", "Kiwi", "Pomme", "Raisin"};
+        // MenuCreator.displayMenu(menu, "---- MENU 1 ----");
+        // int choix = MenuCreator.readChoice(menu);
+        // System.out.println("Vous avez choisi : " + menu[choix - 1]);
+
+
+        // choix = MenuCreator.displayMenuAndReadChoice(menu, "---- MENU 2 ----");
+        // System.out.println("Vous avez choisi : " + menu[choix - 1]);
 
 
     }
